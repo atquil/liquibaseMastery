89b5ba502f1c4401f808494b24054698c6e21e11
ATQUIL <creator.atquil@gmail.com>
2026-01-09 07:57:39 +0530

    first commit

---
+++ src/main/java/com/atquil/liquibasemastery/LiquibaseMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.atquil.liquibasemastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseMasteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseMasteryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atquil/liquibasemastery/LiquibaseMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atquil.liquibasemastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseMasteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
